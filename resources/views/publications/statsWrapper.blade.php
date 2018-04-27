9c343899427c556f4ba8d9fe13cd15b3e3ec44ad
jcontreras28 <janellec@bendbroadband.com>
2018-04-27 15:16:32 -0700

    adding ajax calls

--- resources/views/publications/statsWrapper.blade.php
+++ resources/views/publications/statsWrapper.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container" id="{{$pubData->id}}">
+<div class="container pubIdContainer" id="{{$pubData->id}}">
 
     <h2>Stats for {{$pubData->name}}  {{$pubData->GAProfileId }}</h2>
 
