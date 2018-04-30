fb34cc2f2b63d20d772601dc9f091b37dae57ae5
vagrant <vagrant@homestead>
2018-04-30 23:06:08 +0000

    section display

--- resources/views/publications/storyStats.blade.php
+++ resources/views/publications/storyStats.blade.php
@@ -4,11 +4,7 @@
 		<ul class="extra-margin-top no-bullet-list">
 
 			@foreach($results['errors'] as $error) 
-				<li class="alert alert-danger" role="alert">{{ $error }}
-					@if (array_key_exists('GAReturn', $error))
-						 - {{ print_r($results['errors']['GAReturn'])}}
-					@endif
-				</li>
+				<li class="alert alert-danger" role="alert">{{ $error }}</li>
 			@endforeach
 
 		</ul>
