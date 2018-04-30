7d586fb303b60c7b3acdfbee3ec930d4773ed96b
vagrant <vagrant@homestead>
2018-04-30 20:57:40 +0000

    section display

--- resources/views/publications/sectionStats.blade.php
+++ resources/views/publications/sectionStats.blade.php
@@ -10,62 +10,7 @@
 		</ul>
 	@endif
 
-        <ul>
-			<li class="list-group-item">
-				<div class="row title">
-					<div class="col-xs-1">
-						Views
-					</div>
-					<div class="col-xs-1">
-						Uniques
-					</div>
-					<div class="col-xs-7">
-						Section Url
-					</div>
-					<div class="col-xs-3">
-						Referrers (top 5)
-					</div>
-
-				</div>
-			</li>
-		</ul>
-
-		<ol class="custom-counter sectionDataList">
-			@foreach($results["sections"] as $section)
-				@if ($loop->index > 200) 
-					@break
-				@endif
-				
-				<li class="list-group-item">
-                    <div class="row">
-                        <div class="col-xs-1">
-                            {{ $section['Views']}}
-                        </div>
-                        <div class="col-xs-1">
-                            {{ $section['Uniques'] }}
-                        </div>
-                        <div class="col-xs-7">
-                            {{ $section['Url'] }}
-                        </div>
-                        <div class="referrer-row col-xs-3">
-
-                            @foreach($section['referrers'] as $subKey => $ref)
-                                @if ($loop->index == 0)
-                                    {{ $ref }} - {{ $subKey }}
-                                @else	
-                                    | {{ $ref }} - {{ $subKey }}
-                                @endif
-                                @if ($loop->index > 5)
-                                    @break
-                                @endif
-                            @endforeach
-                        </div>
-                    </div>
-                </li>
-			@endforeach
-		</ol>
-
-
+        
 		<pre>
 		{{ print_r($results) }}
 		</pre>
