567f7889c8c3ec8faeef7e9f24448b108c52278f
vagrant <vagrant@homestead>
2018-04-30 19:33:16 +0000

    added views

---
+++ resources/views/publications/sectionStats.blade.php
@@ -0,0 +1,76 @@
+@extends('layouts.app')
+
+@section('content')
+	
+	@if (array_key_exists('errors', $results)) 
+		<ul class="extra-margin-top no-bullet-list">
+
+			@foreach($results['errors'] as $error) 
+				<li class="alert alert-danger" role="alert">{{ $error }}</li>
+			@endforeach
+
+		</ul>
+	@endif
+
+        <ul>
+			<li class="list-group-item">
+				<div class="row title">
+					<div class="col-xs-1">
+						Views
+					</div>
+					<div class="col-xs-1">
+						Uniques
+					</div>
+					<div class="col-xs-7">
+						Section Url
+					</div>
+					<div class="col-xs-3">
+						Referrers (top 5)
+					</div>
+
+				</div>
+			</li>
+		</ul>
+
+		<ol class="custom-counter sectionDataList">
+			@foreach($results["sections"] as $section)
+				@if ($loop->index > 200) 
+					@break
+				@endif
+				
+				<li class="list-group-item">
+                    <div class="row">
+                        <div class="col-xs-1">
+                            {{ $section['Views']}}
+                        </div>
+                        <div class="col-xs-1">
+                            {{ $section['Uniques'] }}
+                        </div>
+                        <div class="col-xs-7">
+                            {{ $section['Url'] }}
+                        </div>
+                        <div class="referrer-row col-xs-3">
+
+                            @foreach($section['referrers'] as $subKey => $ref)
+                                @if ($loop->index == 0)
+                                    {{ $ref }} - {{ $subKey }}
+                                @else	
+                                    | {{ $ref }} - {{ $subKey }}
+                                @endif
+                                @if ($loop->index > 5)
+                                    @break
+                                @endif
+                            @endforeach
+                        </div>
+                    </div>
+                </li>
+			@endforeach
+		</ol>
+
+
+		<pre>
+		{{ print_r($results) }}
+		</pre>
+</div>
+
+@endsection
