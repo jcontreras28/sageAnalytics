1b39fac143a898bcf0a8141f9252b7d4380852d4
vagrant <vagrant@homestead>
2018-05-01 17:48:49 +0000

    sorting referrers

--- resources/views/publications/storyStats.blade.php
+++ resources/views/publications/storyStats.blade.php
@@ -40,6 +40,7 @@
 						<div class="referrer-row col-xs-3" style="font-size:.9em">
 							<span id='storyRefs{{ $loop->index }}'>
 								
+								{{ krsort($story['referrers']) }}
 								@foreach($story['referrers'] as $subKey => $ref)
 									@if ($loop->index == 0)
 										{{ $ref }} - {{ $subKey }}
