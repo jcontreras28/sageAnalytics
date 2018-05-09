36e6446b1727f2e64054e1b0020f4ec5cb912661
vagrant <vagrant@homestead>
2018-05-09 23:33:46 +0000

    working on realtime

--- resources/views/publications/realTime.blade.php
+++ resources/views/publications/realTime.blade.php
@@ -9,71 +9,7 @@
 		</li>
 	</ul>
 
-	<div class="col-sm-6">
-		<h3>Top 20 stories right now:</h3>
-		<div id="realTimeStories">
-            <ul>
-                <li class="list-group-item">
-                    <div class="row title">
-                        <div class="col-xs-1">Count</div>
-                        <div class="col-xs-11">Headline, name and author</div>
-                    </div>
-                </li>
-            </ul>
-            <ol class="custom-counter">
+	
 
-                @foreach($resultsRealtime as $story)
-                <li class="list-group-item list-group-item-ordered">
-                    <div class="row">
-                        <div class="col-xs-1">
-                            {{ $story['count'] }}
-                        </div>
-                        <div class="col-xs-11">
-                            @if (array_key_exists('image', $story) && $story['image'] != 'none') 
-								<img class='pull-right storyImage' width='80px' src='{{ $story["image"] }}'/ >
-							@endif
-                            <span class='storyHeadlineBold'> {{ $story['realHeadline'] }}</span><br>
-                            <span>{{ $story['name'] }}</span><br>
-                            <a href='{{ $story["link"] }}' target='_blank'>View story</a>
-                        </div>
-                    </div>
-                </li>
-                @endforeach
-            </ol>
-        </div>
-	</div>
-
-	<div class="col-sm-6">
-		<h3>Top 20 for day:</h3>
-		<div id="curDayStories">
-            <ul>
-                <li class="list-group-item">
-                    <div class="row title">
-                        <div class="col-xs-1">Count</div>
-                        <div class="col-xs-11">Headline, name and author</div>
-                    </div>
-                </li>
-            </ul>
-            <ol class="custom-counter">
-
-                @foreach($results['stories'] as $story)
-                    <li class="list-group-item list-group-item-ordered">
-                        <div class="row">
-                            <div class="col-xs-1">
-                                {{ $story['count'] }}
-                            </div>
-                            <div class="col-xs-11">
-                                @if (array_key_exists('image', $story) && $story['image'] != 'none') 
-                                    <img class='pull-right storyImage' width='80px' src='{{ $story["image"] }}'/ >
-                                @endif
-                                <span class='storyHeadlineBold'> {{ $story['realHeadline'] }}</span><br>
-                                <span>{{ $story['name'] }}</span><br>
-                                <a href='{{ $story["link"] }}' target='_blank'>View story</a>
-                            </div>
-                        </div>
-                    </li>
-                @endforeach
-            </ol>
-        </div>
-	</div>
+	
 </div>
