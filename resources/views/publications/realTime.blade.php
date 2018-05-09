71d1fe33551469aaca3611ee0e2d82f4dd481749
vagrant <vagrant@homestead>
2018-05-09 23:42:46 +0000

    working on realtime

--- resources/views/publications/realTime.blade.php
+++ resources/views/publications/realTime.blade.php
@@ -25,23 +25,7 @@
                 @if (array_key_exists('errors', $resultsRealtime)) 
                     <h2>Errror: {{ $resultsRealtime['error'] }}
                 @else
-                    @foreach( $resultsRealtime as $story)
-                    <li class="list-group-item list-group-item-ordered">
-                        <div class="row">
-                            <div class="col-xs-1">
-                                {{ $story['count'] }}
-                            </div>
-                            <div class="col-xs-11">
-                                @if (array_key_exists('image', $story) && $story['image'] != 'none') 
-                                    <img class='pull-right storyImage' width='80px' src='{{ $story["image"] }}'/ >
-                                @endif
-                                <span class='storyHeadlineBold'> {{ $story['headline'] }}</span><br>
-                                <span>{{ $story['name'] }}</span><br>
-                                <a href='{{ $story["link"] }}' target='_blank'>View story</a>
-                            </div>
-                        </div>
-                    </li>
-                    @endforeach
+                    
                 @end
             </ol>
         </div>
