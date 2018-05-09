9d496b682a70a40b653fd33aa2bb4d7380fd066b
vagrant <vagrant@homestead>
2018-05-09 23:36:09 +0000

    working on realtime

--- resources/views/publications/realTime.blade.php
+++ resources/views/publications/realTime.blade.php
@@ -38,26 +38,7 @@
                     </div>
                 </li>
             </ul>
-            <ol class="custom-counter">
-
-                @foreach($results['stories'] as $story)
-                    <li class="list-group-item list-group-item-ordered">
-                        <div class="row">
-                            <div class="col-xs-1">
-                                {{ $story['count'] }}
-                            </div>
-                            <div class="col-xs-11">
-                                @if (array_key_exists('image', $story) && $story['image'] != 'none') 
-                                    <img class='pull-right storyImage' width='80px' src='{{ $story["image"] }}'/ >
-                                @endif
-                                <span class='storyHeadlineBold'> {{ $story['realHeadline'] }}</span><br>
-                                <span>{{ $story['name'] }}</span><br>
-                                <a href='{{ $story["link"] }}' target='_blank'>View story</a>
-                            </div>
-                        </div>
-                    </li>
-                @endforeach
-            </ol>
+            
         </div>
 	</div>
 </div>
