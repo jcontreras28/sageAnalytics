c7c1eff2050c3e37164ec5d892bd77355fd0ec62
vagrant <vagrant@homestead>
2018-05-10 20:21:23 +0000

    shortening realtime refresh time

--- public/js/thisapp.js
+++ public/js/thisapp.js
@@ -102,7 +102,10 @@ $(document).ready( function() {
 
     setInterval(function(){
         getStorySectionData('0daysAgo', 'today');
-        getRealTimeData();
-	}, 120000);
+    }, 120000);
+    
+    setInterval(function(){
+		getRealTimeData();
+	}, 15000);
 
 });
